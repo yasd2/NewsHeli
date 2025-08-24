efd2f53cef820407c75d753a09557f427094c317
yasd2 <158278728+yasd2@users.noreply.github.com>
2025-08-24 17:46:09 +0200

    changed EnableVan to false

--- Utils/Config.cs
+++ Utils/Config.cs
@@ -9,7 +9,7 @@ internal class Config
     public static UInt32 ArrivalTimeHeli { get; set; } = 30;
     public static UInt32 ArrivalTimeVan { get; set; } = 40;
     public static Boolean EnableHeli { get; set; } = true;
-    public static Boolean EnableVan { get; set; } = true;
+    public static Boolean EnableVan { get; set; } = false;
 
 
     /// <summary>
